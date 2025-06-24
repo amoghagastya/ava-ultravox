a6a64c00e25555b6d3cbd71ee1c3b590ffc31db9
Amogh Agastya <amogh.agastya@builder.ai>
2025-06-24 16:08:05 +0530

    added new GTM tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "AVA Voice Assistant Demo",
-  description: "Engage in a live, conversational voice experience with AVA—Amogh’s Virtual Assistant—right from the hero section of the site."
+  description: "Engage in a live, conversational voice experience with AVA—Amogh's Virtual Assistant—right from the hero section of the site."
 };
 
 export default function RootLayout({
@@ -19,11 +19,11 @@ export default function RootLayout({
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-W4XTN42P');
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M52X4K37');
             `,
           }}
         />
@@ -31,14 +31,12 @@ export default function RootLayout({
       </head>
       <body>
         {/* Google Tag Manager (GTM) NoScript START */}
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-W4XTN42P"
-            height="0" 
-            width="0" 
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-M52X4K37"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
         {/* Google Tag Manager (GTM) NoScript END */}
         {children}
         <Analytics />
