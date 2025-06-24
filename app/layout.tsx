9adf29d89261b1b0c5d42eab120766141a8fa88e
Amogh Agastya <amogh.agastya@builder.ai>
2025-06-24 15:14:06 +0530

    add GTM code snippet

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}
-      <Analytics />
+      <head>
+        {/* Google Tag Manager (GTM) Head Script START */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W4XTN42P');
+            `,
+          }}
+        />
+        {/* Google Tag Manager (GTM) Head Script END */}
+      </head>
+      <body>
+        {/* Google Tag Manager (GTM) NoScript START */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W4XTN42P"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* Google Tag Manager (GTM) NoScript END */}
+        {children}
+        <Analytics />
       </body>
     </html>
   )
